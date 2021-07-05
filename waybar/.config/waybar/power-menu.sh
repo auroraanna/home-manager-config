96d6a80ff52ad9b0d97278377e4623e63537047b
papojari <papojari-git.ovoid@aleeas.com>
2021-07-05 03:57:40 +0200

    add rofi power menu

---
+++ waybar/.config/waybar/power-menu.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+LOCK=""
+LOGOUT=""
+SUSPEND=" Suspend"
+HIBERNATE="  Hibernate"
+HYBRID_SLEEP=" Hybrid sleep"
+RESTART=""
+SHUTDOWN=""
+
+list_icons() {
+    echo $LOCK
+	echo $LOGOUT
+    echo $SUSPEND
+	echo $HIBERNATE
+	echo $HYBRID_SLEEP
+    echo $RESTART
+    echo $SHUTDOWN
+}
+
+handle_option() {
+    case "$1" in
+        "$LOCK")
+          swaylock -f -c 000000
+            ;;
+        "$LOGOUT")
+          swaymsg exit
+            ;;
+        "$SUSPEND")
+          systemctl suspend
+            ;;
+		"$HIBERNATE")
+          systemctl hibernate
+            ;;
+		"$HYBRID_SLEEP")
+          systemctl hybrid-sleep
+            ;;
+        "$RESTART")
+          reboot
+            ;;
+        "$SHUTDOWN")
+          shutdown now
+            ;;
+    esac
+}
+
+SELECTION="$(list_icons | rofi -dmenu -theme options_menu)"
+handle_option $SELECTION &
