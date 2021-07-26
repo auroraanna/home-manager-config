53a94b73a4f79fa9b5fc561da73165438f872633
papojari <papojari-git.ovoid@aleeas.com>
2021-07-26 18:02:12 +0200

    fix formatting (identation)

--- waybar/.config/waybar/power-menu.sh
+++ waybar/.config/waybar/power-menu.sh
@@ -9,39 +9,39 @@ RESTART=" Restart"
 SHUTDOWN=" Shutdown"
 
 list_icons() {
-    echo $LOCK
+	echo $LOCK
 	echo $LOGOUT
-    echo $SUSPEND
+	echo $SUSPEND
 	echo $HIBERNATE
 	echo $HYBRID_SLEEP
-    echo $RESTART
-    echo $SHUTDOWN
+	echo $RESTART
+	echo $SHUTDOWN
 }
 
 handle_option() {
-    case "$1" in
-        "$LOCK")
-          swaylock -f -c 000000
-            ;;
-        "$LOGOUT")
-          swaymsg exit
-            ;;
-        "$SUSPEND")
-          systemctl suspend
-            ;;
+	case "$1" in
+		"$LOCK")
+		swaylock -f -c 000000
+		;;
+		"$LOGOUT")
+		swaymsg exit
+		;;
+		"$SUSPEND")
+		systemctl suspend
+		;;
 		"$HIBERNATE")
-          systemctl hibernate
-            ;;
+		systemctl hibernate
+		;;
 		"$HYBRID_SLEEP")
-          systemctl hybrid-sleep
-            ;;
-        "$RESTART")
-          reboot
-            ;;
-        "$SHUTDOWN")
-          shutdown now
-            ;;
-    esac
+		systemctl hybrid-sleep
+		;;
+		"$RESTART")
+		reboot
+		;;
+		"$SHUTDOWN")
+		shutdown now
+		;;
+	esac
 }
 
 SELECTION="$(list_icons | rofi -dmenu -theme options_menu)"
