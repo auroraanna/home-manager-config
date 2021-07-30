a288dda8e7b8b7b5c1cdcd3bb81f0b8d8aaec92a
papojari <papojari-git.ovoid@aleeas.com>
2021-07-30 20:55:33 +0200

    enable gpu acceleration via hardware options not via bootloader because the bootloader required for it to work is not supported on raspberry pi 4

--- system/configuration-rpi4.nix
+++ system/configuration-rpi4.nix
@@ -25,16 +25,16 @@
 			generic-extlinux-compatible.enable = false;
 		};
 		kernelPackages = pkgs.linuxPackages_rpi4;
-    	tmpOnTmpfs = true;
+    		tmpOnTmpfs = true;
 		initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
 		# ttyAMA0 is the serial console broken out to the GPIO
-    	kernelParams = [
+    		kernelParams = [
 	        "8250.nr_uarts=1"
 	        "console=ttyAMA0,115200"
 	        "console=tty1"
 	        # Some gui programs need this
 	        "cma=128M"
-    	];
+    		];
 	};
 
 
@@ -45,7 +45,11 @@
 		#	driSupport32Bit = true;
 		#};
 		# Enable GPU acceleration
-		#raspberry-pi."4".fkms-3d.enable = true;
+		raspberry-pi."4".fkms-3d = {
+      		enable = lib.mkEnableOption ''
+       			Enable modesetting through fkms-3d
+      		'';
+		};
 		# Required for the Wireless firmware
 		enableRedistributableFirmware = true;
 	};
