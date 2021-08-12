1a085319b8ae13dc54e5f2cd930a977a722d6005
papojari <papojari-git.ovoid@aleeas.com>
2021-08-12 22:32:36 +0200

    source sway scripts

--- users/default/sway/default.nix
+++ users/default/sway/default.nix
@@ -2,4 +2,7 @@
 
 {
   home.file.".config/sway/config".source = ./config;
+  home.file.".config/sway/screenshot.sh".source = ./screenshot.sh;
+  home.file.".config/sway/scan-barcode.sh".source = ./scan-barcode.sh;
+  home.file.".config/sway/color-picker.sh".source = ./color-picker.sh;
 }
