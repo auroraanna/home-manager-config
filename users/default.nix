0302e996d4f806ebf62e1e102cbce825158720dc
papojari <papojari-git.ovoid@aleeas.com>
2021-08-13 22:09:58 +0200

    remove defining shell per user and instead define a global shell

--- users/default.nix
+++ users/default.nix
@@ -13,7 +13,6 @@
         "video"
         "lp"
       ];
-      shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILcgisTGLBwRrhdmOIEkFP4K2IjI0KuJ4UoOOdn4KNcy papojari@Cryogonull"
       ];
