516ff630489f70caa1358e8b783083e5966b5c2b
papojari <papojari-git.ovoid@aleeas.com>
2021-08-22 14:02:55 +0200

    add more default apps

--- users/default/xdg.nix
+++ users/default/xdg.nix
@@ -6,10 +6,21 @@
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "image" = [ "gthumb.desktop" ];
-        "text" = [ "codium.desktop" ];
+        "text/plain" = [ "codium.desktop" ];
         "x-scheme-handler/http" = [ "brave.desktop" ];
         "x-scheme-handler/https" = [ "brave.desktop" ];
+        "image/webp" = [ "org.gnome.gThumb.desktop" ];
+        "image/png" = [ "org.gnome.gThumb.desktop" ];
+        "image/jpeg" = [ "org.gnome.gThumb.desktop" ];
+        "video/mp4" = [ "org.gnome.Totem.desktop;mpv.desktop" ];
+        "video/webm" = [ "org.gnome.Totem.desktop;mpv.desktop" ];
+        "audio/mpeg" = [ "org.gnome.Totem.desktop;mpv.desktop" ];
+        "audio/mp4" = [ "org.gnome.Totem.desktop;mpv.desktop" ];
+        "audio/x-wav" = [ "org.gnome.Totem.desktop;mpv.desktop" ];
+        "audio/x-opus+ogg" = [ "org.gnome.Totem.desktop;mpv.desktop" ];
+        "audio/flac" = [ "org.gnome.Totem.desktop;mpv.desktop" ];
+        "application/json" = [ "codium.desktop" ];
+        "application/pdf" = [ "brave-browser.desktop;org.gnome.Evince.desktop" ];
       };
     };
   };
