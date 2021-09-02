f2102416ea4bf012e899ace9250fc56d1ae92030
papojari <papojari-git.ovoid@aleeas.com>
2021-09-02 20:29:08 +0200

    add drive mount and umount buttons to waybar

--- users/default/sway/default.nix
+++ users/default/sway/default.nix
@@ -2,7 +2,8 @@
 
 {
   home.file.".config/sway/config".source = ./config;
-  home.file.".config/sway/screenshot.sh".source = ./screenshot.sh;
+  home.file.".config/sway/drive-mount.sh".source = ./drive-mount.sh;
+  home.file.".config/sway/drive-unmount.sh".source = ./drive-unmount.sh;
   home.file.".config/sway/scan-barcode.sh".source = ./scan-barcode.sh;
   home.file.".config/sway/color-picker.sh".source = ./color-picker.sh;
 }
