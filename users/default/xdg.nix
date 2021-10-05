f8b7f8cf08a8e1e9c74c3c5da5a14fe649ac1ade
papojari <papojari-git.ovoid@aleeas.com>
2021-10-05 23:54:29 +0200

    configure firefox

--- users/default/xdg.nix
+++ users/default/xdg.nix
@@ -7,8 +7,16 @@
       enable = true;
       defaultApplications = {
         "text/plain" = [ "codium.desktop" ];
-        "x-scheme-handler/http" = [ "brave-browser.desktop" ];
-        "x-scheme-handler/https" = [ "brave-browser.desktop" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+        "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+        "text/html" = [ "firefox.desktop" ];
+        "application/x-extension-htm" = [ "firefox.desktop" ];
+        "application/x-extension-html" = [ "firefox.desktop" ];
+        "application/x-extension-shtml" = [ "firefox.desktop" ];
+        "application/xhtml+xml" = [ "firefox.desktop" ];
+        "application/x-extension-xhtml" = [ "firefox.desktop" ];
+        "application/x-extension-xht" = [ "firefox.desktop" ];
         "image/svg" = [ "brave-browser.desktop" ];
         "image/webp" = [ "org.gnome.gThumb.desktop" ];
         "image/png" = [ "org.gnome.gThumb.desktop" ];
@@ -21,7 +29,7 @@
         "audio/x-opus+ogg" = [ "mpv.desktop" ];
         "audio/flac" = [ "mpv.desktop" ];
         "application/json" = [ "codium.desktop" ];
-        "application/pdf" = [ "brave-browser.desktop;org.gnome.Evince.desktop" ];
+        "application/pdf" = [ "firefox.desktop;org.gnome.Evince.desktop" ];
       };
     };
   };
