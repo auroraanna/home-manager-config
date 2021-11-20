015d6165be7e00b43692d17e90744a85db9e9ed9
papojari <papojari-git.ovoid@aleeas.com>
2021-11-20 15:38:35 +0100

    update packages: remove unused, add new ones

--- packages/default.nix
+++ packages/default.nix
@@ -10,7 +10,7 @@
       # Languages
       zsh rustc jdk
       # CLI
-      tmux bat wget kakoune git git-crypt git-lfs gnupg pinentry-gnome age tealdeer stow pandoc youtube-dl ytfzf librespeed-cli sftpman zbar imagemagick xorg.xeyes wev sox trash-cli pinentry-curses exa zoxide fd ripgrep bat-extras.batdiff bat-extras.batgrep bat-extras.batman bat-extras.batwatch bat-extras.prettybat monolith
+      tmux bat wget git git-crypt git-lfs gnupg age tealdeer youtube-dl ytfzf librespeed-cli zbar imagemagick xorg.xeyes wev sox trash-cli pinentry-curses exa zoxide fd ripgrep bat-extras.batdiff bat-extras.batgrep bat-extras.batman bat-extras.batwatch bat-extras.prettybat monolith ventoy-bin
       ## Compression tools
       zstd zpaq
       ## Prompts
@@ -22,21 +22,19 @@
       ## Fun
       asciiquarium cmatrix nyancat cbonsai toilet figlet cowsay lolcat cava
       # Video and image
-      gthumb mpv mpvScripts.youtube-quality ffmpeg
+      gthumb mpv ffmpeg
       # Wayland, Xorg
-      wl-clipboard polkit polkit_gnome waybar ulauncher wofi wofi-emoji slurp grim swappy swaylock-fancy notify-desktop mako libappindicator gnome.zenity
+      wl-clipboard polkit polkit_gnome
       # Other
-      alacritty gnome.gnome-tweak-tool gnome.gnome-shell-extensions deja-dup gnome.dconf-editor transmission-gtk
+      alacritty deja-dup transmission-gtk
       # File browsers
-      gnome.nautilus cinnamon.nemo xplr
+      cinnamon.nemo
       # Web browsers
       firefox-wayland
       # Vulkan
       vulkan-loader vulkan-tools
       # Filesystem stuff
-      gparted dosfstools mtools ntfs3g btrfs-progs sshfs jmtpfs
-      # Python
-      python39Packages.pyqrcode python39Packages.spotipy python39Packages.pytz
+      gparted dosfstools mtools ntfs3g btrfs-progs jmtpfs
     ];
   };
 }
