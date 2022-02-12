7af095f3e84f1aa2837051bad2a5544ebaec2b29
papojari <papojari-git.ovoid@aleeas.com>
2022-02-12 15:41:00 +0100

    feat(starship): enable integration for all shells

--- users/default/starship/default.nix
+++ users/default/starship/default.nix
@@ -1,5 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableIonIntegration = true;
+    enableZshIntegration = true;
+  };
   home.file.".config/starship.toml".source = ./starship.toml;
 }
