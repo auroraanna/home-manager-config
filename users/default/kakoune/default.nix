0855f4def7517b7cbe6f8a5169072ec727f1fa95
papojari <papojari-git.ovoid@aleeas.com>
2022-02-28 21:26:28 +0100

    feat(kakoune): add smarttab plugin

--- users/default/kakoune/default.nix
+++ users/default/kakoune/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  home.file.".config/kak/smarttab.kak".source = builtins.fetchurl {
+    url = https://raw.githubusercontent.com/andreyorst/smarttab.kak/master/rc/smarttab.kak;
+    sha256 = "1xqljpv4jy7c42yh4h4xp8r3j3k0xn20yagi0dscdv2gpgqgsjn3";
+  };
   home.file.".config/kak/copy_to_system_clipboard.kak".source = ./copy_to_system_clipboard.kak;
   home.file.".config/kak/disable_auto_indent.kak".source = ./disable_auto_indent.kak;
 
--- users/default/kakoune/sources.kak
+++ users/default/kakoune/sources.kak
@@ -1,2 +1,3 @@
 source "%val{config}/copy_to_system_clipboard.kak"
 source "%val{config}/disable_auto_indent.kak"
+source "%val{config}/smarttab.kak"
