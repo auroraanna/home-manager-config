307671acc81949fc6af9f9fae74fe812cf2d8d86
papojari <papojari-git.ovoid@aleeas.com>
2022-04-17 01:21:04 +0200

    refactor(waybar): use material-dark-yellow theme

--- users/default/waybar/default.nix
+++ users/default/waybar/default.nix
@@ -1,11 +1,12 @@
 { config, pkgs, lib, ... }:
 
 let
-  colorScheme = import ../../../color-schemes/gruvbox-dark-hard.nix;
+  colorScheme = import ../../../color-schemes/material-dark-yellow.nix;
   scratchpadIndicatorScript = builtins.fetchurl {
     url = https://raw.githubusercontent.com/justinesmithies/sway-dotfiles/master/.local/bin/statusbar/scratchpad-indicator.sh;
     sha256 = "0xv2w1512gv31sbnwd1grdhrcvzngn8ljdj3x61mqgcqfcp57mwz";
   };
+  font = "Roboto";
 in {
   home.packages = with pkgs; [ jq radeontop ];
 
@@ -147,7 +148,7 @@ in {
         };
         "custom/power" = {
           format = "";
-          on-click = "swaynag --border-bottom-size=3 --message-padding=8 --button-border-size=5 --button-padding=8 --background=${colorScheme.blackBright} --border-bottom=${colorScheme.black} --button-border=${colorScheme.white} --button-background=${colorScheme.white} -f Roboto -t warning -m 'Power Menu Options' -b '⏻︁ Power off'  'shutdown -P now' -b '↻︁ Restart' 'shutdown -r now' -b '🛌︁ Hibernate' 'systemctl hibernate' -b '🛌︁ Hybrid-sleep' 'systemctl hybrid-sleep' -b '🛌︁ Suspend' 'systemctl suspend' -b '︁ Logout' 'swaymsg exit' -b ' Lock' 'swaylock-fancy -f Roboto'";
+          on-click = "swaynag --border-bottom-size 0 --details-border-size 0 --button-border-size 0 --button-margin 5 --message-padding=8 --button-padding=8 --background=${colorScheme.background} --text=${colorScheme.on_background} --button-background=${colorScheme.surface_variant} --button-text=${colorScheme.on_surface_variant} -f ${font} -t warning -m 'Power Menu Options' -b '⏻︁ Power off'  'shutdown -P now' -b '↻︁ Restart' 'shutdown -r now' -b '🛌︁ Hibernate' 'systemctl hibernate' -b '🛌︁ Hybrid-sleep' 'systemctl hybrid-sleep' -b '🛌︁ Suspend' 'systemctl suspend' -b '︁ Logout' 'swaymsg exit' -b ' Lock' 'swaylock-fancy -f ${font}'";
           #on-click = "sh $HOME/.config/waybar/power-menu.sh";
         };
         "custom/gpu" = {
@@ -209,8 +210,8 @@ in {
     };
     style = "
       * {
-        border-radius: 0.6em;
-        font-family: 'Roboto', 'Font Awesome';
+        border-radius: 0.4rem;
+        font-family: '${font}', 'Font Awesome';
         font-size: 13px;
         min-height: 0;
       }
@@ -218,23 +219,20 @@ in {
       #window, #workspaces button.urgent, #workspaces button.focused, .workspaces button, #mode, #custom-power, #tray, #clock, #battery, #cpu, #custom-gpu, #memory, #disk, #temperature, #backlight, #network, #pulseaudio, #custom-media, #idle_inhibitor, #custom-drive-mount, #custom-drive-unmount, #custom-screenshot, #custom-scan-barcode, #custom-color-picker, #custom-emoji-picker, #custom-copy-date, #custom-scratchpad-indicator {
         padding: 0 10px;
         margin: 0 3px;
-        border-bottom-width: 3px;
-        border-bottom-style: solid;
-        background-color: #${colorScheme.whiteBright};
-        border-color: #${colorScheme.white};
-        color: #${colorScheme.black};
+        background-color: #${colorScheme.background};
+        color: #${colorScheme.on_background};
       }
 
       /*window.waybar {
       }*/
 
       #window {
-        background-color: #${colorScheme.greenBright};
-        border-color: #${colorScheme.green}
+        background-color: #${colorScheme.primary};
+        color: #${colorScheme.on_primary};
       }
 
       #workspaces {
-        color: #${colorScheme.whiteBright};
+        color: #${colorScheme.on_background};
       }
 
       /* https://github.com/Alexays/Waybar/wiki/FAQ#the-workspace-buttons-have-a-strange-hover-effect */
@@ -244,60 +242,49 @@ in {
 
       #workspaces button.focused {
         padding: 0 0px;
-        background-color: #${colorScheme.blueBright};
-        border-color: #${colorScheme.blue};
+        background-color: #${colorScheme.surface_variant};
+        color: #${colorScheme.on_surface_variant};
       }
 
       #workspaces button.urgent {
         padding: 0 0px;
-        background-color: #${colorScheme.redBright};
-        border-color: #${colorScheme.red};
-      }
-
-      #mode {
-        background-color: #343447;
-        border-color: #171725;
-        color: #ffffff;
+        background-color: #${colorScheme.error};
+        color: #${colorScheme.on_error};
       }
 
       #custom-power {
         margin-left: 0;
         padding: 0 12px;
-        background-color: #${colorScheme.yellowBright};
-        border-color: #${colorScheme.yellow};
-      }
-
-      #tray {
-        background-color: #${colorScheme.whiteBright};
-        border-color: #${colorScheme.white};
+        background-color: #${colorScheme.primary_container};
+        color: #${colorScheme.on_primary_container};
       }
 
       #clock {
         margin-right: 0;
-        background-color: #${colorScheme.whiteBright};
-        border-color: #${colorScheme.white};
+        background-color: #${colorScheme.surface_variant};
+        color: #${colorScheme.on_surface_variant};
       }
 
       #battery {
-        background-color: #${colorScheme.whiteBright};
-        border-color: #${colorScheme.white};
+        background-color: #${colorScheme.secondary};
+        color: #${colorScheme.on_secondary};
       }
 
       #battery.charging {
-        background-color: #${colorScheme.yellowBright};
-        border-color: #${colorScheme.yellow};
+        background-color: #${colorScheme.primary};
+        color: #${colorScheme.on_primary};
       }
 
       @keyframes blink {
         to {
-          background-color: #${colorScheme.greenBright};
-          border-color: #${colorScheme.green}
+          background-color: #${colorScheme.tertiary};
+          color: #${colorScheme.on_tertiary};
         }
       }
 
       #battery.critical:not(.charging) {
-        background-color: #${colorScheme.redBright};
-        border-color: #${colorScheme.red};
+        background-color: #${colorScheme.error};
+        color: #${colorScheme.on_error};
         animation-name: blink;
         animation-duration: 0.5s;
         animation-timing-function: linear;
@@ -306,114 +293,78 @@ in {
       }
 
       #cpu {
-        background-color: #${colorScheme.blueBright};
-        border-color: #${colorScheme.blue};
+        background-color: #${colorScheme.tertiary};
+        color: #${colorScheme.on_tertiary};
       }
 
       #custom-gpu {
-        background-color: #${colorScheme.blueBright};
-        border-color: #${colorScheme.blue};
+        background-color: #${colorScheme.tertiary};
+        color: #${colorScheme.on_tertiary};
       }
 
       #memory {
-        background-color: #${colorScheme.aquaBright};
-        border-color: #${colorScheme.aqua};
-      }
-
-      #disk {
-        background-color: #${colorScheme.blackBright};
-        border-color: #${colorScheme.black};
-        color: #${colorScheme.whiteBright};
-      }
-
-      #backlight {
-        background-color: #${colorScheme.whiteBright};
-        border-color: #${colorScheme.white};
+        background-color: #${colorScheme.tertiary_container};
+        color: #${colorScheme.on_tertiary_container};
       }
 
       #network {
-        background-color: #${colorScheme.yellowBright};
-        border-color: #${colorScheme.yellow};
+        background-color: #${colorScheme.secondary};
+        color: #${colorScheme.on_secondary};
       }
 
       #network.disconnected {
-        background-color: #${colorScheme.redBright};
-        border-color: #${colorScheme.red};
+        background-color: #${colorScheme.error};
+        color: #${colorScheme.on_error};
       }
 
       #pulseaudio {
-        background-color: #${colorScheme.magentaBright};
-        border-color: #${colorScheme.magenta};
+        background-color: #${colorScheme.secondary};
+        color: #${colorScheme.on_secondary};
       }
 
       #pulseaudio.muted {
-        background-color: #${colorScheme.blackBright};
-        border-color: #${colorScheme.black};
-        color: #${colorScheme.white};
+        background-color: #${colorScheme.surface_variant};
+        color: #${colorScheme.on_surface_variant};
       }
 
       #temperature {
-        background-color: #${colorScheme.magentaBright};
-        border-color: #${colorScheme.magenta};
+        background-color: #${colorScheme.tertiary};
+        color: #${colorScheme.on_tertiary};
       }
 
       #temperature.critical {
-        background-color: #${colorScheme.redBright};
-        border-color: #${colorScheme.red};
-      }
-
-      #idle_inhibitor {
-        background-color: #${colorScheme.blackBright};
-        border-color: #${colorScheme.black};
-        color: #${colorScheme.whiteBright};
+        background-color: #${colorScheme.error};
+        color: #${colorScheme.on_error};
       }
 
       #idle_inhibitor.activated {
-        background-color: #${colorScheme.greenBright};
-        border-color: #${colorScheme.green};
+        background-color: #${colorScheme.tertiary};
+        color: #${colorScheme.on_tertiary};
       }
 
       #custom-screenshot {
-        background-color: #${colorScheme.blueBright};
-        border-color: #${colorScheme.blue};
+        background-color: #${colorScheme.tertiary_container};
+        color: #${colorScheme.on_tertiary_container};
       }
 
       #custom-drive-mount {
-        background-color: #${colorScheme.greenBright};
-        border-color: #${colorScheme.green};
+        background-color: #${colorScheme.tertiary};
+        color: #${colorScheme.on_tertiary};
       }
 
       #custom-drive-unmount {
-        background-color: #${colorScheme.redBright};
-        border-color: #${colorScheme.red};
+        background-color: #${colorScheme.error_container};
+        color: #${colorScheme.on_error_container};
       }
 
       #custom-scan-barcode {
-        background-color: #${colorScheme.whiteBright};
-        border-color: #${colorScheme.white};
+        background-color: #${colorScheme.on_background};
+        color: #${colorScheme.background};
       }
 
       #custom-color-picker {
-        background-color: #${colorScheme.blueBright};
-        border-color: #${colorScheme.blue};
-      }
-
-      #custom-emoji-picker {
-        background-color: #${colorScheme.blackBright};
-        border-color: #${colorScheme.black};
-        color: #${colorScheme.whiteBright};
-      }
-
-      #custom-copy-date {
-        background-color: #${colorScheme.blackBright};
-        border-color: #${colorScheme.black};
-        color: #${colorScheme.whiteBright};
-      }
-
-      #custom-scratchpad-indicator {
-        background-color: #${colorScheme.blackBright};
-        border-color: #${colorScheme.black};
-        color: #${colorScheme.whiteBright};
+        background-color: #${colorScheme.primary};
+        color: #${colorScheme.on_primary};
       }
     ";
   };
