31cf9e4113c754efdb6c845aecff68331d2f796d
papojari <papojari-git.ovoid@aleeas.com>
2022-04-30 03:11:50 +0200

    git: set Commit as default commit message editor

--- users/default/git.nix
+++ users/default/git.nix
@@ -4,14 +4,13 @@
   programs = {
     git = {
       enable = true;
-      signing = {
-        signByDefault = true;
-      };
+      signing = { signByDefault = true; };
       lfs.enable = true;
       delta.enable = true;
       extraConfig = {
         init.defaultBranch = "main";
         tag.gpgSign = true;
+        core.editor = "flatpak run --file-forwarding re.sonny.Commit @@";
       };
     };
   };
